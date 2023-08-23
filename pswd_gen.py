ea5b8e0b26251eb7df764aca3b9996ea6dc05514
Aurelie Gedeon <agedeon540@gmail.com>
2023-08-23 16:25:58 -0500

    set up prompt for password generator

--- pswd_gen.py
+++ pswd_gen.py
@@ -32,6 +32,11 @@ def generate_password(min_length, numbers=True, special_characters=True):
             meets_criteria = has_number
         if special_characters:
             meets_criteria = meets_criteria and has_special
+    return pswd
 
 
-generate_password(10)
+min_length = int(input("Enter the minimum length: "))
+has_number = input("Do you want it to have numbers? (y/n)").lower() == "y"
+has_special = input(
+    "Do you want it to have special characters? (y/n)").lower() == "y"
+print(pswd)
