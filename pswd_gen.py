125aab1e9b62ea5cb24191b16eef3544210ea2f5
Aurelie Gedeon <agedeon540@gmail.com>
2023-08-23 16:09:15 -0500

    set up while loop for random characters

--- pswd_gen.py
+++ pswd_gen.py
@@ -15,6 +15,17 @@ def generate_password(min_length, numbers=True, special_characters=True):
 
     pswd = ""
     meets_criteria = False
+    has_number = False
+    has_special = False
+
+    while not meets_criteria or len(pswd) < min_length:
+        new_char = random.choice(characters)
+        pswd += new_char
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
 
 
 generate_password(10)
